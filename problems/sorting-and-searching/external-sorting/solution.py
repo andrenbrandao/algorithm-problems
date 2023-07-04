bb50b642f2c54c238ba336d9a8e0ce6e51691696
André Brandão <andrenbrandao@gmail.com>
2023-07-03 21:04:08 -0300

    sorting-and-searching: add parallelized version to external sorting

--- problems/sorting-and-searching/external-sorting/solution.py
+++ problems/sorting-and-searching/external-sorting/solution.py
@@ -1,30 +1,43 @@
+import multiprocessing
+import sys
+import time
+from joblib import Parallel, delayed
+from heapq import heappush, heappop
+
 LINES_PER_CHUNK = 1000000
+is_parallel = False
 
-from heapq import heapify, heappush, heappop
+if len(sys.argv) > 1:
+    if "--parallel" in sys.argv[1]:
+        is_parallel = True
 
+
+# Method to sort one chunk using python's sort
 def sort_chunk(number):
     numbers = []
     filename = f"chunk_{number}.txt"
 
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         for line in file:
-            numbers.append(int(line.strip('\n')))
-
+            numbers.append(int(line.strip("\n")))
 
     numbers.sort()
 
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         for number in numbers:
             file.write(f"{number}\n")
 
 
-with open('large_file.txt', 'r') as file:
+t = time.time()
+
+# Reads the large file and breaks it into chunks
+with open("large_file.txt", "r") as file:
     chunk_number = 0
     count_lines = 0
 
     line = file.readline()
     while line:
-        with open(f"chunk_{chunk_number}.txt", 'w') as chunk_file:
+        with open(f"chunk_{chunk_number}.txt", "w") as chunk_file:
             while line and count_lines <= LINES_PER_CHUNK:
                 chunk_file.write(line)
                 count_lines += 1
@@ -33,24 +46,31 @@ with open('large_file.txt', 'r') as file:
         count_lines = 0
         chunk_number += 1
 
+# Execute the sorting of each chunk
+if is_parallel:
+    num_cores = multiprocessing.cpu_count()
+    Parallel(n_jobs=num_cores)(delayed(sort_chunk)(i) for i in range(chunk_number))
+else:
+    for i in range(chunk_number):
+        sort_chunk(i)
 
-for i in range(chunk_number):
-    sort_chunk(i)
-
+# Get the file descriptors for each chunk
 chunk_files = []
 for i in range(chunk_number):
-    f = open(f"chunk_{i}.txt", 'r')
+    f = open(f"chunk_{i}.txt", "r")
     chunk_files.append(f)
 
+# Push the first elements of each chunk to the heap
 minheap = []
-
 for i, file in enumerate(chunk_files):
     line = file.readline()
 
     if line:
-        value = int(line.strip('\n'))
+        value = int(line.strip("\n"))
         heappush(minheap, (value, i))
 
+# Reads the chunks and add the values to the heap
+# Writing the sorted values to the result file
 with open("result.txt", "w") as file:
     while minheap:
         value, chunk_pos = heappop(minheap)
@@ -59,9 +79,11 @@ with open("result.txt", "w") as file:
 
         line = chunk_files[chunk_pos].readline()
         if line:
-            new_value = int(line.strip('\n'))
+            new_value = int(line.strip("\n"))
             heappush(minheap, (new_value, chunk_pos))
 
+# Closes the file descriptors
 for chunk_file in chunk_files:
     chunk_file.close()
 
+print(time.time() - t)
